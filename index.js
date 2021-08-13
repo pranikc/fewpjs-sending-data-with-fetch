6f84a00a4037e4dee4d6632e1ff9ffb322ac3286
pranik chainani <pranikc@gmail.com>
2021-08-13 10:06:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)   
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(r => r.json())
+        .then(data => document.querySelector('body').textContent = data["id"] )
+        .catch(error => document.querySelector('body').textContent = error.message)
+}
